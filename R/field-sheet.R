385c6068993e0bdd2c8a35f0c3f5e923aae02a67
ergz <erodriguez@flowwest.com>
2022-09-30 11:29:30 -0700

    fixes the way we call `create_field_sheet`
    the error came from the order in which were passing the arguments.
    After the update, it did error out on add_style due to mismatch in
    length between rows and cols, fixed by using same the same range for
    both.

--- R/field-sheet.R
+++ R/field-sheet.R
@@ -23,7 +23,7 @@ create_field_sheet <- function(wb, field_sheet_sample_plan,
   pageSetup(wb, sheet = sheet_name, orientation = "landscape", left = 0.5, right = 0.25,
             top = 0.75, bottom = 0.75, printTitleRows = 1, fitToWidth = TRUE)
   setColWidths(wb, sheet_name, cols = 1:10, widths = c(3, 11, 8, 12, 10, 10, 8, 8, 8, 37))
-  addStyle(wb, sheet = sheet_name, style = col_style, rows = row_range, cols = 1:4)
+  addStyle(wb, sheet = sheet_name, style = col_style, rows = row_range, cols = row_range)
   writeData(wb, sheet = sheet_name, field_sheet_sample_plan, borders = "all", borderColour = "#000000",
             headerStyle = col_header)
   setHeaderFooter(wb, sheet = sheet_name, header = c(NA, center_header_text, right_header_text))
@@ -34,8 +34,8 @@ create_field_sheet <- function(wb, field_sheet_sample_plan,
 data <- tibble(
   Bin = "A",
   `Bin FL Range (mm)` = "25-53",
-  `Sample #` = 1:30,
-  `Sample ID` = paste0("BTC22_3_A_", 1:30),
+  `Sample #` = 1:120,
+  `Sample ID` = paste0("BTC22_3_A_", 1:120),
   Date = "",
   Time = "",
   `FL (mm)` = "",
@@ -48,6 +48,9 @@ first_sample_date <- lubridate::as_date("2022-02-08")
 sample_event_number <- 3
 
 wb <- createWorkbook()
-wb <- create_field_sheet(wb, "BTC-3", data, 3, sample_location = "Battle Creek", "BTC")
+wb <- create_field_sheet(wb = wb, sample_event_number = 3,
+                         field_sheet_sample_plan = data,
+                         first_sample_date = first_sample_date,
+                         sample_location = "Battle Creek", "BTC")
 saveWorkbook(wb, "test.xlsx", overwrite = TRUE)
 
