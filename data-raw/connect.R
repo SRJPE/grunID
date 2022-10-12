d258ba9c43fe27f99b4e5f48eebb4dbf70059154
ergz <erodriguez@flowwest.com>
2022-10-11 22:10:21 -0700

    updates to files to test run type assignment

--- data-raw/connect.R
+++ data-raw/connect.R
@@ -10,18 +10,12 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
                  user = cfg$username,
                  password = cfg$password)
 
+# running this does the following:
+# add_sample_events(con, sample_plan)
+# add_sample_bins(con, sample_plan, sample_event_ids)
+# add_samples(con, sample_plan, sample_id_insert)
+# set_sample_status(con, sample_ids, "created")
 test <- add_sample_plan(con, grunID::sample_plan_template)
-
-a <- get_sample_status(con, sample_ids)
-View(a)
-sample_ids_to_set <- sample_ids[1:8]
-
-set_sample_status(con, sample_ids = sample_ids_to_set, comment = "sent to feather river",
-                  status = "out to field")
-
-a <- get_sample_status(con, sample_ids_to_set)
-get_sample_status(con, sample_ids_to_set, TRUE) |> View()
-View(b)
 
 # select protocol
 all_protocols <- get_protocols(con)
@@ -29,18 +23,26 @@ all_protocols <- get_protocols(con)
 View(all_protocols) # review available protocols and select appropriate protocol
 protocol_id <- all_protocols[1, "id", drop = TRUE]
 
-plate_run_uid <- grunID::add_plate_run(con, protocol_id, genetic_method_id = 1,
-                                       laboratory_id, lab_work_preformed_by)
+# get lab id
+laboratory_id <- get_laboratories(con) |>
+  filter(stringr::str_detect(code, "DWR")) |> pull(id)
+
+plate_run_uid <- add_plate_run(con,
+                               date_run = "2022-01-01",
+                               protocol_id = protocol_id,
+                               genetic_method_id = 1,
+                               laboratory_id = laboratory_id,
+                               lab_work_preformed_by = "user")
 
 # sample layout
-layout <- grunID::process_well_sample_details("data-raw/well_sample_template.csv")
-
-# run sherlock
+layout <- process_well_sample_details("data-raw/well_sample_template.csv",
+                                      plate_run_id = plate_run_uid)
 
 # process
-results <- grunID::process_sherlock(sherlock_results_filepath = "data-raw/exampleoutput_synergyH1trial_data_092021.xlsx",
-                            sample_layout_mapping = layout,
-                            plate_size = 96)
+results <- process_sherlock(
+  filepath = "data-raw/exampleoutput_synergyH1trial_data_092021.xlsx",
+  sample_details = layout,
+  plate_size = 96)
 
 grunID::add_assay_results(con, results)
 
