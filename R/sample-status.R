9113b8bb2c912bdc4485e87f4b7d837c542e45ea
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-02-22 21:46:29 -0800

    adds progress to sample_status

--- R/sample-status.R
+++ R/sample-status.R
@@ -33,7 +33,11 @@ set_sample_status <- function(con, sample_ids, status_code_id, comment = NULL) {
   is_valid_connection(con)
 
 
-  status_codes_to_insert <- rep(status_code_id, length(sample_ids))
+  if (length(sample_ids) != length(status_code_id)) {
+    status_codes_to_insert <- rep(status_code_id, length(sample_ids))
+  } else {
+    status_codes_to_insert <- status_code_id
+  }
 
   if (!is.null(comment)) {
     comments <- rep(comment, length(sample_ids))
@@ -54,7 +58,12 @@ set_sample_status <- function(con, sample_ids, status_code_id, comment = NULL) {
                                         .con = con)
   }
 
-  total_inserts <- purrr::map_dbl(sample_status_query, \(q) DBI::dbExecute(con, q))
+  total_inserts <- purrr::map_dbl(sample_status_query, \(q) DBI::dbExecute(con, q),
+                                  .progress = list(
+                                    type = "iterator",
+                                    name = "updating status codes for uploaded samples",
+                                    clear = FALSE)
+                                  )
 
   return(sum(total_inserts))
 }
