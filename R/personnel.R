fa9fef20df95ee5775e23f93a21c011fad40ca67
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-06-28 14:17:32 -0700

    adds more personnel function and docs

--- R/personnel.R
+++ R/personnel.R
@@ -12,7 +12,7 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
 #' Get all personnel
 #' @param con DBI connection object to database
 #' @param collected boolean indidcated whether the returned object should be collected from database
-#' or returned as a lazy postgres dataframe.
+#' or returned as a postgres lazy_query.
 #' @param ... additional named arguments passed into dplyr::filter,
 #' use this add filter to be executed on the database before collecting.
 #' @export
@@ -26,4 +26,33 @@ get_personnel <- function(con, collected=TRUE, ...) {
     return(d)
   }
 }
+
+#' Add personnel to table
+#' @param con connection to database
+#' @param first_name first name for personnel
+#' @param last_name last name for personnel
+#' @param laboratory_id the id for lab personnel is associated with
+#' @export
+add_personnel <- function(con, first_name, last_name, laboratory_id) {
+  d <- data.frame(first_name = first_name, last_name = last_name, laboratory_id = laboratory_id)
+  res <- DBI::dbAppendTable(con, "lab_personnel", d)
+  return(res)
+}
+
+#' Remove personnel
+#' @param con connection to the database
+#' @param id id for record to remove
+#' @details
+#' Records are not actually deleted via this API, instead they are just set to inactive.
+#' @export
+delete_personnel <- function(con, id) {
+  q <- glue::glue_sql(
+    "UPDATE lab_personnel SET active = false WHERE id = {id};",
+    .con = con
+  )
+
+  res <- DBI::dbExecute(con, q)
+  return(res)
+}
+
 
