d0f15288b9a4558b37ac2eb3aa58d0d8de1f0a3f
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-06-29 11:30:59 -0700

    fixes the way we handle assay types in the add_genetic_id function
    basically we must first filter to just the plate run at hand, then
    we can make use of the `names_expand` argument to pivot so that we dont
    have to do the hacky "DELETE_ME" row. Note that to do this we must first
    make the assay_type into a factor to let `name_expand` know there are
    other values not present here that need to be expanded.

--- R/load-sherlock.R
+++ R/load-sherlock.R
@@ -63,7 +63,7 @@ generate_threshold <- function(con, plate_run_identifier, .control_id="NTC") {
 #' @returns The number of assay results added to the assay_result table
 #' and the number of samples updated in the genetic_run_identification table.
 #' @export
-update_assay_detection <- function(con, thresholds, .control_id = "NTC") {
+update_assay_detection <- function(con, thresholds, plate_run_id, .control_id = "NTC") {
 
   if (!DBI::dbIsValid(con)) {
     stop("Connection argument does not have a valid connection the run-id database.
@@ -114,7 +114,7 @@ update_assay_detection <- function(con, thresholds, .control_id = "NTC") {
   ))
 
 
-  genetic_ids_added <- add_genetic_identification(con, unique(detection_results$sample_id))
+  genetic_ids_added <- add_genetic_identification(con, unique(detection_results$sample_id), plate_run_id = plate_run_id)
 
   return(c("Assay records added" = sum(assay_results_added),
            "Samples assigned run type" = genetic_ids_added))
@@ -210,32 +210,29 @@ add_raw_assay_results <- function(con, assay_results) {
 #' database is updated with the genetic identification value. The genetic
 #' identification values are dependent on the assay results:
 #' 1: high value for
-add_genetic_identification <- function(con, sample_identifiers) {
+add_genetic_identification <- function(con, sample_identifiers, plate_run_id) {
 
   is_valid_connection(con)
 
   assay_detections <- tbl(con, "assay_result") |>
-    dplyr::filter(sample_id %in% sample_identifiers) |>
+    dplyr::filter(sample_id %in% sample_identifiers, plate_run_id == !!plate_run_id) |>
     dplyr::select(sample_id, assay_id, positive_detection) |>
     dplyr::collect() |>
     dplyr::mutate(assay_id_name = case_when(assay_id == 1 ~ "ots_28_e",
                                             assay_id == 2 ~ "ots_28_l",
                                             assay_id == 3 ~ "ots_16_s",
-                                            assay_id == 4 ~ "ots_16_w")) |>
+                                            assay_id == 4 ~ "ots_16_w"),
+                  assay_id_name = factor(assay_id_name, levels = c("ots_28_e","ots_28_l","ots_16_s","ots_16_w"))) |>
     dplyr::select(-assay_id) |>
-    tidyr::pivot_wider(names_from = "assay_id_name", values_from = "positive_detection")
+    tidyr::pivot_wider(names_from = "assay_id_name", values_from = "positive_detection", names_expand = TRUE)
 
   if (nrow(assay_detections) == 0) {
     return(0)
   }
 
-  run_types <- dplyr::bind_rows(
-    tibble::tibble(sample_id = "DELETE_ME", ots_28_e = FALSE, ots_28_l = FALSE, ots_16_s = FALSE, ots_16_w = FALSE),
-    assay_detections
-  ) |>
+  run_types <- assay_detections |>
     assign_status_codes() |>
     assign_run_types() |>
-    dplyr::filter(sample_id != "DELETE_ME") |>
     dplyr::select(sample_id, run_type_id, status_code_id)
 
   spring_winter <- run_types |>
