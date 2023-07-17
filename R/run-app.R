da1c0099948a1af4fe2d66a23ab8d4bbf521f1f9
liz-fw <lstebbins@flowwest.com>
2023-07-17 08:54:14 -0700

    adds docs for run_app()

--- R/run-app.R
+++ R/run-app.R
@@ -1,7 +1,17 @@
-#' Launch
+#' @title Launch App
+#' @description `run_app()` launches a shiny application that allows the user to
+#' upload data to the run ID database.
+#' @details the grunID application can be run when the user has a completed plate run
+#' with assay results. This application calls `add_new_plate_results()`.
+#' @examples
+#' grunID::run_app()
+#' @md
 #' @export
 run_app <- function(x, ...)
 {
   shiny::runApp(appDir = system.file("app", package = "grunID"),
                 ...)
 }
+
+
+
