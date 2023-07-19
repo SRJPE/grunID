e4d9bd93e6d41ad24d7a218a86dc48ddc783914f
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-07-19 15:05:01 -0700

    adds a check for dev env

--- inst/app/global.R
+++ inst/app/global.R
@@ -5,14 +5,21 @@ library(shinyBS)
 library(DT)
 
 config_path <- Sys.getenv("CONFIG_PATH")
-cfg <- config::get(file = config_path)
-con <- DBI::dbConnect(RPostgres::Postgres(),
-                      dbname = cfg$dbname,
-                      host = cfg$host,
-                      port = 5432,
-                      user = cfg$username,
-                      password = cfg$password)
+in_dev_mode <- as.logical(Sys.getenv("GRUNID_IS_DEV"))
 
+if (!is.na(in_dev_mode) && in_dev_mode) {
+
+  cfg <- config::get(file = config_path)
+  con <- DBI::dbConnect(RPostgres::Postgres(),
+                        dbname = cfg$dbname,
+                        host = cfg$host,
+                        port = 5432,
+                        user = cfg$username,
+                        password = cfg$password)
+
+} else {
+  con <- grunID::gr_db_connect()
+}
 
 all_protocols <- get_protocols(con) |> collect()
 all_labs <- get_laboratories(con) |> select(id, code, laboratory_name, description) |> collect()
