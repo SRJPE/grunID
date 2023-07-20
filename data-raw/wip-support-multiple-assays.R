32449a9fca3eb67283fe1503732875b469ef5075
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-07-20 16:11:06 -0700

    more work in progress

--- data-raw/wip-support-multiple-assays.R
+++ data-raw/wip-support-multiple-assays.R
@@ -39,9 +39,115 @@ first_assay <- tbl(con, "assay_result") |>
   collect()
 
 
+ots_early_late_detection <- function(con, sample_id,
+                                     strategy = c("positive priority", "recent priority")) {
+
+  strategy <- match.arg(strategy)
+
+  assay_results <- tbl(con, "assay_result") |>
+    filter(sample_id == !!sample_id)
+
+  assays_for_existing_for_sample <- assay_results |> dplyr::distinct(assay_id) |> dplyr::pull()
+
+  assay_needed_not_found <- which(!(1:2 %in% assays_for_existing_for_sample ))
+
+  if (length(assay_needed_not_found)) {
+    cli::cli_abort(c(
+      "x" = "late and early assay needed to run early/late detection but assay = {assay_needed_not_found} was not found for sample {sample_id}"
+    ))
+  }
+
+  ots_early <- assay_results |> filter(assay_id == 1)
+
+  if (nrow(collect(ots_early)) > 1) {
+    cli::cli_alert_info("using '{strategy}' to identify a unique assay run")
+    if (strategy == "positive priority") {
+      ots_early_priority_results <-
+        ots_early |> filter(positive_detection) |>
+        collect()
+      if (nrow(ots_early_priority_results) > 1) {
+        cli::cli_abort(c(
+          "x" = "'positive priority' did not identify a unique assay run",
+          "i" = "try a different strategy or pass plate id of run to use for identification"
+        ))
+      }
+    } else if (strategy == "recent priority") {
+      ots_early_priority_results <-
+        ots_early |> arrange(desc(created_at)) |>
+        head(1) |>
+        collect()
+    }
+  } else {
+    ots_early_priority_results <- collect(ots_early)
+  }
+
+
+
+
+  ots_late <- assay_results |> filter(assay_id == 2)
+
+  if (nrow(collect(ots_late)) > 1) {
+    cli::cli_alert_info("using '{strategy}' to identify a unique assay run")
+    if (strategy == "positive priority") {
+      ots_late_priority_results <-
+        ots_late |> filter(positive_detection) |>
+        collect()
+      if (nrow(ots_late_priority_results) > 1) {
+        cli::cli_abort(c(
+          "x" = "'positive priority' did not identify a unique assay run",
+          "i" = "try a different strategy or pass plate id of run to use for identification"
+        ))
+      }
+    } else if (strategy == "recent priority") {
+      ots_late_priority_results <-
+        ots_late |> arrange(desc(created_at)) |>
+        head(1) |>
+        collect()
+    }
+  } else {
+    ots_late_priority_results <- collect(ots_late)
+  }
+
+  if (!ots_early_priority_results$positive_detection && ots_late_priority_results$positive_detection) {
+    # sample is fall or late-fall
+    return(list(sample_Id = sample_id, status_code = "analysis complete", run_type="FAL"))
+  } else if (ots_early_priority_results$positive_detection && ots_late_priority_results$positive_detection) {
+    return(list(sample_Id = sample_id, status_code = "analysis complete", run_type="UNK"))
+  } else if (ots_early_priority_results$positive_detection && !ots_late_priority_results$positive_detection) {
+    return(list(sample_Id = sample_id, status_code = "need ots16", run_type = NA))
+  } else {
+    return(list(sample_Id = sample_id, status_code = NA, run_type = NA))
+  }
+
+}
+
+safe_version <- safely(ots_early_late_detection)
+out <- map(ids, ~safe_version(con, ., strategy = "recent"))
+
+out_2 <- out |> transpose()
+
+out_2$result
+
+
+w <- safe_version(con, "F1722_3_A_7", strategy = "recent")
+
+x <- safely(\(x) cli::cli_abort(c("fdsafdsa")))
+y <- x(10)
+
+
+
+microbenchmark::microbenchmark(
+  suppressMessages(map(c("F1722_3_A_1", "F1722_3_A_2"), ~ots_early_late_detection(con, ., strategy = "recent")))
+)
+
+
+
+
 run_assay_detection <- function(con, sample_id, first_plate = NULL, second_plate = NULL) {
   assay_results <- tbl(con, "assay_result") |>
     filter(sample_id == !!sample_id)
+
+  if (all(c(1, 2)))
 
   first_assay <- assay_results |>
     filter(assay_id == 1) |>
@@ -54,25 +160,36 @@ run_assay_detection <- function(con, sample_id, first_plate = NULL, second_plate
 
   total_first_assay <- nrow(first_assay)
   if (total_first_assay > 1) {
-    cli::cli_alert_warning("more than one assay found, please pass additional argument")
-    cli::cli_alert_info("use the following to identify which plate runs to use:")
-    code_msg <- glue::glue("grunID::get_plate_run(con, id %in% c({glue::glue_collapse(first_assay$plate_run_id, sep = ', ')}))")
-    cli::cli_code(code_msg)
+    code_msg <- glue::glue("use the following to identify which plate runs to use: grunID::get_plate_run(con, id %in% c({glue::glue_collapse(first_assay$plate_run_id, sep = ', ')}))")
+    cli::cli_abort(c(
+      "x" = "more than one assay found, please pass additional argument run species identification",
+      "i" = code_msg
+    ))
   }
 
   second_assay <- assay_results |>
     filter(assay_id == 2) |>
     collect()
 
-  if (nrow(second_assay) ==1) {
+  if (nrow(second_assay) == 0) {
     cli::cli_abort(c(
       "x" = "{.var sample_id} = {sample_id} does not have a second assay associated with it"
     ))
   }
 
+  if (nrow(second_assay) > 1) {
+    code_msg <- glue::glue("use the following to identify which plate runs to use: grunID::get_plate_run(con, id %in% c({glue::glue_collapse(second_assay$plate_run_id, sep = ', ')}))")
+    cli::cli_abort(c(
+      "x" = "more than one assay found, please pass additional argument run species identification",
+      "i" = code_msg
+    ))
+  }
+
+
+
 }
 
-run_assay_detection(con, "F1722_3_A_1", first_plate = 83)
+run_assay_detection(con, "F1722_3_A_1",                   = 83)
 
 
 
