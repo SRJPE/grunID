3e75f5fa1ba77ab153041a5522b2975e7ef1164b
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-01-03 11:17:22 -0800

    excel files to use for testing

---
+++ data-raw/sherlock-example-outputs/2024/ebk-ids/EBK-scratch.R
@@ -0,0 +1,25 @@
+cfg <- config::get()
+con <- DBI::dbConnect(RPostgres::Postgres(),
+               dbname = cfg$dbname,
+               host = cfg$host,
+               port = 5432,
+               user = cfg$username, password = cfg$password)
+
+library(grunID)
+add_new_plate_results(
+  con = con,
+  protocol_name = "default",
+  genetic_method = "SHLK",
+  laboratory = "DWR_GeM",
+  lab_work_performed_by = "Emanuel",
+  description = "test",
+  date_run = lubridate::today(),
+  filepath = "data-raw/sherlock-example-outputs/2024/ebk-ids/061223_JPE24_E+L_E1-2_P1_SH_RH.xlsx",
+  sample_type = "fin_clip",
+  layout_type = "split_plate_early_late",
+  plate_size = 384,
+  .control_id = "EBK",
+  selection_strategy = "recent priority",
+  run_gen_id = TRUE
+  # samples_source = "jpe"
+)
