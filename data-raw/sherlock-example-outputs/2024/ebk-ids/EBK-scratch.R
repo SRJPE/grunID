d448f6a27be445d05b2e363387861e022780e273
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-01-05 08:33:24 -0800

    more failing file examples

--- data-raw/sherlock-example-outputs/2024/ebk-ids/EBK-scratch.R
+++ data-raw/sherlock-example-outputs/2024/ebk-ids/EBK-scratch.R
@@ -14,7 +14,7 @@ add_new_plate_results(
   lab_work_performed_by = "Emanuel",
   description = "test",
   date_run = lubridate::today(),
-  filepath = "data-raw/sherlock-example-outputs/2024/061223_JPE24_E+L_E1-2_P1_SH_RH - FAIL.xlsx",
+  filepath = "data-raw/sherlock-example-outputs/2024/ebk-ids/061223_JPE24_E+L_E1-2_P1_SH_RH - FAIL.xlsx",
   sample_type = "fin_clip",
   layout_type = "split_plate_early_late",
   plate_size = 384,
@@ -23,3 +23,14 @@ add_new_plate_results(
   run_gen_id = TRUE
   # samples_source = "jpe"
 )
+
+
+text <- "MANUAL EBK VALUE CHECK: plate_run_id = 393 Values = EBK-1(13000);EBK-2(13000)"
+matches <- str_match_all(text, "EBK-\\d+\\((\\d+)\\)")
+values <- matches[[1]][, 1]
+
+matches <- str_match_all(text, "EBK-\\d+-\\d+\\((\\d+)\\)")
+values <- matches[[1]]
+
+library(stringr)
+text <- "MANUAL EBK VALUE CHECK: plate_run_id = 393 Values = EBK-1-1(13000);EBK-3-2(14000)"
