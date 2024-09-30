f7dadec18b436372287873a12ae4c822973d9e07
ergz <erodriguez@flowwest.com>
2024-09-30 11:38:18 -0700

    wip - insert plate to archive id
    @ergz this needs a table to be created on the database

--- R/plate-maps.R
+++ R/plate-maps.R
@@ -11,7 +11,7 @@ partition_df_every_n <- function(df, n) {
 }
 
 #' @title Create Plate Layout
-#' @descriotion creates a plate layout dataframe by a given layout size
+#' @description creates a plate layout dataframe by a given layout size
 #' @param samples vector of samples to create map for
 #' @param layout_size the size of the layout to create
 #' @keywords internal
@@ -29,12 +29,20 @@ make_plate_map <- function(df) {
   purrr::map(df, \(x) make_plate_layout(x$id))
 }
 
+#' @title Create Plate Maps for Archive Plates
+#' @param con a database connection
+#' @param events a vector of events to generate plate maps for
+#' @param season a season to produce plates for, uses current year be default
 #' @export
-make_plate_maps_by_event <- function(con, events, season = lubridate::year(lubridate::today())) {
+make_archive_plate_maps_by_event <- function(con, events, season = lubridate::year(lubridate::today())) {
   season_filter <- stringr::str_sub(season, -2)
   samples <- con |> tbl("sample") |>
     filter(event_number %in% events, season == season_filter) |>
     collect()
+
+  if (nrow(samples) == 0) {
+    stop("no samples found that match events and season combination", call. = FALSE)
+  }
 
   events_name <- paste(events, collapse="-")
 
@@ -45,20 +53,37 @@ make_plate_maps_by_event <- function(con, events, season = lubridate::year(lubri
 
   message(glue::glue("A total of {nrow(samples)} samples were arranged into {length(layouts_list)} plates"))
 
-  for (i in seq_along(layouts_list)) {
-    subplate_num <- if ((x <- i %% 4) == 0) 4 else x
-    if (i > 4) {
-      subplate_num <- paste0(subplate_num, "-1")
-    }
-    filename <- glue::glue("sherlock_{events_name}_p{subplate_num}.xlsx")
-    write_layout_to_file(layouts_list[[i]], filename)
-    message(paste(filename, "file created"))
-  }
+  filenames <- glue::glue("JPE{season_filter}_E{events_name}_P{seq_along(layouts_list)}_ARC.xlsx")
+  purrr::walk(seq_along(layouts_list), function(i) {
+    write_layout_to_file(layouts_list[[i]], filenames[i])
+    message(paste(filenames[i], "file created"))
 
+  })
 
-  invisible(layouts_list)
+  names(layouts_list) <- tools::file_path_sans_ext(filenames)
+
+  out <- layouts_list|>
+    enframe(name = "archive_plate_id") |>
+    unnest(cols = value) |>
+    pivot_longer(cols = -archive_plate_id,
+                 names_to = NULL,
+                 values_to = "sample_id") |>
+    filter(!is.na(sample_id)) # remove intentional blanks
+
+  invisible(out)
 }
 
+#' @export
+insert_archive_plate_ids <- function(con, archive) {
+  sql_statement <- glue::glue_sql("
+  INSERT INTO sample_archive_plates (sample_id, arc_plate_id) values(
+    UNNEST(ARRAY[{archive$sample_id*}]),
+    UNNEST(ARRAY[{archive$archive_plate_id*}])
+  ) ON CONFLICT (sample_id, arc_plate_id)
+  DO UPDATE SET arc_plate_id = EXCLUDED.arc_plate_id;", .con = con)
+
+  DBI::dbExecute(con, sql_statement)
+}
 
 write_layout_to_file <- function(df, file_name) {
   # Create a new workbook
