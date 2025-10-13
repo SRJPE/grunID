0a240c4b4f66c67dc1c72ec542debcb4a76e461f
liz-fw <lstebbins@flowwest.com>
2025-10-13 14:24:55 -0700

    adds check to raw gt seq insert to make sure samples exist in sample dataset

--- R/gtseq.R
+++ R/gtseq.R
@@ -6,29 +6,49 @@ read_gtseq <- function(filepath) {
 
 #' @export
 insert_gtseq_raw_results <- function(con, gtseq_data) {
+
+  # TODO need a cleaner way to do this
+  sherlock_sample_ids <- tbl(con, "sample") |>
+    select(id) |>
+    collect()
+
+  # data filtered to those with sherlock results in db (in "sample" table)
+  insert_data <- gtseq_data |>
+    filter(SampleID %in% sherlock_sample_ids$id)
+
   values_clause <- paste(sprintf("('%s', %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                                 gtseq_data$SampleID, gtseq_data$Gtseq_Chr28_Geno, gtseq_data$Pop_Structure_ID,
-                                 gtseq_data$CV_Fall, gtseq_data$CV_Late_Fall, gtseq_data$CV_Spring,
-                                 gtseq_data$CV_Winter, gtseq_data$Tributary, gtseq_data$ButteFall,
-                                 gtseq_data$FRHfall, gtseq_data$FRHsp, gtseq_data$MillDeerFall,
-                                 gtseq_data$SanJoaquinFall, gtseq_data$ButteSp, gtseq_data$MillDeerSp,
-                                 gtseq_data$ColemanLF, gtseq_data$SacWin, NA), collapse = ", ")
+                                 insert_data$SampleID,
+                                 insert_data$Gtseq_Chr28_Geno,
+                                 insert_data$Pop_Structure_ID,
+                                 insert_data$CV_Fall,
+                                 insert_data$CV_Late_Fall,
+                                 insert_data$CV_Spring,
+                                 insert_data$CV_Winter,
+                                 insert_data$Tributary,
+                                 insert_data$ButteFall,
+                                 insert_data$FRHfall,
+                                 insert_data$FRHsp,
+                                 insert_data$MillDeerFall,
+                                 insert_data$SanJoaquinFall,
+                                 insert_data$ButteSp,
+                                 insert_data$MillDeerSp,
+                                 insert_data$ColemanLF,
+                                 insert_data$SacWin, NA), collapse = ", ")
 
   insert_statements <- glue::glue_sql("INSERT INTO gtseq_results (sample_id, gtseq_chr28_geno, pop_structure_id, cv_fall, cv_late_fall, cv_spring, cv_winter, tributary, buttefall, frh_fall, frh_sp, mill_deer_fall, san_joaquin_fall, butte_sp, mill_deer_sp, coleman_f, sac_win, season) VALUES
-                                    ( {gtseq_data$SampleID}, {gtseq_data$Gtseq_Chr28_Geno}, {gtseq_data$Pop_Structure_ID},
-                                 {gtseq_data$CV_Fall}, {gtseq_data$CV_Late_Fall}, {gtseq_data$CV_Spring},
-                                 {gtseq_data$CV_Winter}, {gtseq_data$Tributary}, {gtseq_data$ButteFall},
-                                 {gtseq_data$FRHfall}, {gtseq_data$FRHsp}, {gtseq_data$MillDeerFall},
-                                 {gtseq_data$SanJoaquinFall}, {gtseq_data$ButteSp}, {gtseq_data$MillDeerSp},
-                                 {gtseq_data$ColemanLF}, {gtseq_data$SacWin}, {2025} )", .con =con)
+                                     ({insert_data$SampleID}, {insert_data$Gtseq_Chr28_Geno}, {insert_data$Pop_Structure_ID},
+                                     {insert_data$CV_Fall}, {insert_data$CV_Late_Fall}, {insert_data$CV_Spring},
+                                     {insert_data$CV_Winter}, {insert_data$Tributary}, {insert_data$ButteFall},
+                                     {insert_data$FRHfall}, {insert_data$FRHsp}, {insert_data$MillDeerFall},
+                                     {insert_data$SanJoaquinFall}, {insert_data$ButteSp}, {insert_data$MillDeerSp},
+                                     {insert_data$ColemanLF}, {insert_data$SacWin}, {2025} )", .con =con)
 
   res <- map(1:length(insert_statements), function(i) {
-    print(paste0("at step ", i))
-    print(insert_statements[i])
     res <- DBI::dbSendQuery(con, insert_statements[i])
     DBI::dbClearResult(res)
-  })
-   return(res)
+  },
+  .progress = T)
+   #return(res)
 }
 
 
